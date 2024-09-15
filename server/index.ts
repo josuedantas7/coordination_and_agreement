6e6d80410ae0a516c33627a02212aaaa5fa173a4
Josué Dantas <105951027+wDarkRx@users.noreply.github.com>
2024-09-15 00:10:14 -0300

    feat: coordination_and_agreement

--- server/index.ts
+++ server/index.ts
@@ -10,31 +10,52 @@ const inputType = z.object({
     cutMustache: z.boolean(),
     id: z.number(),
 });
-const appRouter = router({
-  cutHair: publicProcedure.input(inputType).query(async (opts) => {
-    const { input } = opts;
-    const cutHair = await db.Barber.cutHair(input);
 
-    return cutHair;
+let isOccupied = false;
+
+async function acquireLock() {
+    while (isOccupied) {
+        await new Promise(resolve => setTimeout(resolve, 100));
+    }
+    isOccupied = true;
+}
+
+function releaseLock() {
+    isOccupied = false;
+}
+
+const appRouter = router({
+  cutHair: publicProcedure.input(inputType).mutation(async (opts) => {
+    const { input } = opts;
+    await acquireLock();
+    console.log(`${input.name} está cortando o cabelo...`);
+    await new Promise(resolve => setTimeout(resolve, 3000));
+    const updatedClient = await db.Barber.cutHair(input);
+    releaseLock();
+    return { success: true, message: `${input.name} cortou o cabelo.`, client: updatedClient };
   }),
-  cutBeard: publicProcedure.input(z.string()).query(async (opts) => {
-    // const { input } = opts;
-    // //      ^?
-    // // Retrieve the user with the given ID
-    // const user = await db.user.findById(input);
-    // return user;
+  
+  cutBeard: publicProcedure.input(inputType).mutation(async (opts) => {
+    const { input } = opts;
+    await acquireLock();
+    console.log(`${input.name} está cortando a barba...`);
+    await new Promise(resolve => setTimeout(resolve, 4000));
+    const updatedClient = await db.Barber.cutBeard(input);
+    releaseLock();
+    return { success: true, message: `${input.name} cortou a barba.`, client: updatedClient };
   }),
-  cutMustache: publicProcedure.input(z.string()).query(async (opts) => {
-//     const { input } = opts;
-//     //      ^?
-//     // Retrieve the user with the given ID
-//     const user = await db.user.findById(input);
-//     return user;
+  
+  cutMustache: publicProcedure.input(inputType).mutation(async (opts) => {
+    const { input } = opts;
+    await acquireLock();
+    console.log(`${input.name} está cortando o bigode...`);
+    await new Promise(resolve => setTimeout(resolve, 5000));
+    const updatedClient = await db.Barber.cutMustache(input);
+    releaseLock();
+    return { success: true, message: `${input.name} cortou o bigode.`, client: updatedClient };
   }),
 });
 
-// Export type router type signature,
-// NOT the router itself.
 export type AppRouter = typeof appRouter;
 
 const server = createHTTPServer({
